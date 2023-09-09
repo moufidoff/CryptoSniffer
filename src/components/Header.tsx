706297cddefbcc92d9900fadeeae3eb30763b910
moufidoff <34994039+moufidoff@users.noreply.github.com>
2023-09-09 19:16:28 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,8 +26,8 @@ const Header: FC<HeaderProps> = ({ hasSearchBar }) => {
     <nav className="z-10 bg-white border-b border-gray-700 dark:bg-gray-800 fixed top-0 left-0 w-full">
       <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
         <a className="flex items-center bg" href="/">
-          <img src="trackdrop.svg" className="h-8 mr-3" alt="TrackDrop Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">TrackDrop</span>
+          <img src="trackdrop.svg" className="h-8 mr-3" alt="SnifferDrop Logo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SnifferDrop</span>
           <a href="https://twitter.com/CryptosSniffer" target="_blank" rel="noopener noreferrer">
             {/* Twitter icon with margin */}
             <FaTwitter size={24} color="#5c86f0" style={{ marginLeft: '430px' }} />
